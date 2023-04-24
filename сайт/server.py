e45747bd029c67514b9a0e0c20f890f154903f08
Вадим <vadimpirogov505@gmail.com>
2023-04-24 20:53:12 +0300

    html-шаблон

--- сайт/server.py
+++ сайт/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Привет, Яндекс!"
+    return render_template('base.html', title='Электронный дневник')
 
 
 if __name__ == '__main__':
