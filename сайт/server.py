7e37577a1d09d41b664f5834a3db0ee6efad8c4d
Вадим <vadimpirogov505@gmail.com>
2023-04-24 21:36:27 +0300

    окно авторизации

--- сайт/server.py
+++ сайт/server.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('base.html', title='Электронный дневник')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/login')
+    return render_template('login.html', title='Электронный дневник', form=form)
 
 
 if __name__ == '__main__':
